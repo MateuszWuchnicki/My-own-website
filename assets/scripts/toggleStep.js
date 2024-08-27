bfbc984480b4de6ad66d8e520400d3052dbafce6
Mateusz Wuchnicki <100024507+MateuszWuchnicki@users.noreply.github.com>
2024-08-27 15:37:53 +0200

    Delete assets directory

--- assets/scripts/toggleStep.js
+++
@@ -1,4 +0,0 @@
-function toggleStep(stepNumber) {
-    const stepElement = document.querySelector(`.infographic-step:nth-child(${stepNumber}) .info-box`);
-    stepElement.classList.toggle('show');
-}
