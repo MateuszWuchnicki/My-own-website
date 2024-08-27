f02dcae938a4287a41ee7c6a92dac697bf4571ef
Mateusz Wuchnicki <100024507+MateuszWuchnicki@users.noreply.github.com>
2024-08-27 16:43:11 +0200

    Update email.js

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,10 +1,24 @@
+// function sendEmail(event) {
+//     event.preventDefault(); // Zapobiega przeładowaniu strony
+
+//     emailjs.sendForm('service_paccnmf', 'template_pqb568o', event.target, 'ptNCneXPkerPcOMwE')
+//     .then((result) => {
+//         alert("Wiadomość wysłana!");
+//     }, (error) => {
+//         alert("Błąd: " + error.text);
+//     });
+// }
+
+
 function sendEmail(event) {
     event.preventDefault(); // Zapobiega przeładowaniu strony
 
     emailjs.sendForm('service_paccnmf', 'template_pqb568o', event.target, 'ptNCneXPkerPcOMwE')
     .then((result) => {
-        alert("Wiadomość wysłana!");
+        alert("Wiadomość została wysłana!");
+        console.log(result.text); // Dla debugowania
     }, (error) => {
         alert("Błąd: " + error.text);
+        console.error(error); // Dla debugowania
     });
 }
