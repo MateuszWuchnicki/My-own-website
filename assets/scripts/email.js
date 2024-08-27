3e80b1ec43e5e1341337acbaf0521ad7ceeaeac4
Mateusz Wuchnicki <100024507+MateuszWuchnicki@users.noreply.github.com>
2024-08-27 17:27:24 +0200

    Update email.js

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,10 +1,44 @@
-function sendMail() {
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    }
-    
-    emailjs.sand('service_paccnmf', 'template_pqb568o', parms).then(alert("Wiadomość została wysłana!"))
-}
+//function sendMail() {
+//    let parms = {
+//        name: document.getElementById("name").value,
+//        email: document.getElementById("email").value,
+//        subject: document.getElementById("subject").value,
+//        message: document.getElementById("message").value,
+//    }
+//    
+//    emailjs.sand('service_paccnmf', 'template_pqb568o', parms).then(alert("Wiadomość została wysłana!"))
+//}
+
+
+
+const form = document.querySelector('form');
+const nameImput = document.getElementById("name");
+const emailImput = document.getElementById("email");
+const messageImput = document.getElementById("message");
+
+const serviceID = 'service_paccnmf';
+const templateID = 'template_i6gg27k';
+const publicKey = 'ptNCneXPkerPcOMwE';
+
+
+emailjs.init(publicKey);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputData = {
+        name:nameImput.value,
+        user_email:emailImput.value,
+        message:messageImput.value,
+    };
+    emailjs.sand(serviceID, templateID, inputData).then(
+        () => {
+            nameImput.value = '';
+            emailImput.value = '';
+            messageImput.value = '';
+            console.log("Wiadomość została wysłana!");
+        },
+        (error)=> {
+            console.log("Error!");
+        }
+    );
+});
