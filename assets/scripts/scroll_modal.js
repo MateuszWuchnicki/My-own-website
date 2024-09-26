74c224f52c0b529ef4ad5c6a8cac968604744d83
Mateusz Wuchnicki <100024507+MateuszWuchnicki@users.noreply.github.com>
2024-09-26 18:26:05 +0200

    Add files via upload

--- assets/scripts/scroll_modal.js
+++ assets/scripts/scroll_modal.js
@@ -1,16 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const scrollToTopButton = document.getElementById('scrollToTopButton');
-    const modal = document.getElementById('modal-scroll');
+    // Znajdujemy wszystkie przyciski przewijania w modalach
+    const scrollButtons = document.querySelectorAll('.scroll-top');
 
-    scrollToTopButton.addEventListener('click', function () {
-        console.log('Przewijanie do góry modalnego okna...');
-        
-        // Przewijanie do góry modalnego okna
-        modal.scrollTo({
-            top: 0,
-            behavior: 'smooth'
+    scrollButtons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            console.log(`Przycisk ${index + 1} kliknięty`);
+
+            // Pobieramy modal na podstawie nadrzędnego elementu
+            const modal = button.closest('.modal').querySelector('.modal-content');
+
+            if (modal) {
+                console.log('Przewijanie do góry modalnego okna...');
+
+                // Przewijanie do góry modalnego okna
+                modal.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+
+                console.log('Obecna pozycja modalnego okna:', modal.scrollTop);
+            } else {
+                console.log('Nie znaleziono modalnego okna dla przycisku', index + 1);
+            }
         });
-
-        console.log('Obecna pozycja modalnego okna:', modal.scrollTop);
     });
 });
